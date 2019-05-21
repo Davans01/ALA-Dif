fa9fa6db62abbe3ea11fb54ecaf893d9c2e696bc
Finley <1040237@mborijnland.nl>
2019-05-21 10:01:34 +0200

    Header and css stuffs

--- include/header.php
+++ include/header.php
@@ -1,10 +1,10 @@
 <header>
     <nav>
         <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="assortiment.php">Assortiment</a></li>
-            <li><a href="contact.php">Login</a></li>
-            <li><a href="bestelling.php">registeren</a></li>
+            <div id="Homebtn"><li><a href="index.php">Home</a></li></div>
+            <div id="Assortimentbtn"><li><a href="assortiment.php">Assortiment</a></li></div>
+            <div id="Loginbtn"><li><a href="contact.php">Login</a></li></div>
+            <div id="Registerbtn"<li><a href="bestelling.php">registeren</a></li><div>
         </ul>
     </nav>
 </header>
