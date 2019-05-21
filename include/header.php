2ddbf996c7a4463c20cbc5a85879d491b05a2766
Finley <1040237@mborijnland.nl>
2019-05-21 10:46:51 +0200

    ok

--- include/header.php
+++ include/header.php
@@ -1,11 +1,10 @@
 <header>
-    <!-- Nav bar -->
     <nav>
         <ul>
             <div id="Homebtn"><li><a href="index.php">Home</a></li></div>
             <div id="Assortimentbtn"><li><a href="assortiment.php">Assortiment</a></li></div>
-            <div id="Loginbtn"><li><a href="contact.php">Login</a></li></div>
-            <div id="Registerbtn"<li><a href="bestelling.php">registeren</a></li><div>
+            <div id="Loginbtn"><li><a href="login.php">Login</a></li></div>
+            <div id="Registerbtn"><li><a href="bestelling.php">registeren</a></li><div>
         </ul>
     </nav>
 </header>
