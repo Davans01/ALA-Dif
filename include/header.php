7007ab3e5b10f1bf18d5bb5921447a3a521d4513
Davans01 <Daniel@maikelvanstraten.nl>
2019-05-21 09:46:32 +0200

    Added a nav bar

---
+++ include/header.php
@@ -0,0 +1,10 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="assortiment.php">Assortiment</a></li>
+            <li><a href="contact.php">Login</a></li>
+            <li><a href="bestelling.php">registeren</a></li>
+        </ul>
+    </nav>
+</header>
